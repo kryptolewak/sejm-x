4f43fdde401139e6c280cc6b7f74bdb4735880b2
kryptolewak <152968031+kryptolewak@users.noreply.github.com>
2023-12-06 14:38:14 +0100

    Dodanie danych o posłach/posłankach

---
+++ scripts/get_mp_data.py
@@ -0,0 +1,32 @@
+import requests
+import csv
+
+# Function to get data from the API
+def get_mp_data(id):
+    url = f"https://api.sejm.gov.pl/sejm/term10/MP/{id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+# Function to save data to CSV
+def save_to_csv(data, filename):
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+
+# Main script to query the API and save the data
+def main():
+    all_data = []
+    for id in range(1, 3):  # IDs range from 1 to 460
+        mp_data = get_mp_data(id)
+        if mp_data:
+            all_data.append(mp_data)
+    
+    # Save to CSV file
+    save_to_csv(all_data, '../csv/sejm_x_mps.csv')
+
+# Run the script
+main()
